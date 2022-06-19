5e1ea6f7867a526e0ed95200eb633a4538349170
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2022-06-19 01:47:47 -0700

    Upsample integers (inefficient), and smoother sky

--- src/shader.fs
+++ src/shader.fs
@@ -8,18 +8,23 @@ in vec2 TexCoord;
 uniform highp usampler2D texture1;
 uniform vec2 iResolution;
 uniform float iTime;
-uniform vec3 camRot;
-uniform vec3 camPos;
+uniform vec3 iCamRot;
+uniform vec3 iCamPos;
 
-const int X = 1024;
-const int Y = 256;
-const int Z = 16;
+const int N = 3;
+const float Nf = 3.;
 
-const float Yf = 256.0;
-const float Zf = 16.0;
+const int X = 1024*N;
+const int Y = 256*N;
+const int Z = 16*N;
 
-const int MAX_RAY_STEPS = 200;
-const int MAX_SUN_STEPS = 5;
+const float Xf = 1024.0*Nf;
+const float Yf = 256.0*Nf;
+const float Zf = 16.0*Nf;
+
+
+const int MAX_RAY_STEPS = 200*N;
+const int MAX_SUN_STEPS = Z;
 
 ivec2 offset(ivec3 c) {
   //center stuff and project down to 2D
@@ -30,7 +35,16 @@ ivec2 offset(ivec3 c) {
 }
 
 ivec3 tex(ivec3 c) {
+<<<<<<<< HEAD:src/shader.fs
   return ivec3(texelFetch(texture1, offset(c), 0));
+========
+  c.x = clamp(c.x + X/2, 0, X-1);
+  c.y = clamp(c.y + Y/2, 0, Y-1);
+  c.z = clamp(c.z, 0, Z-1);
+  c /= N;
+  ivec3 v = ivec3(texelFetch(mapTexture, ivec2(c.x, c.y + Y/N*c.z), 0));
+  return v;
+>>>>>>>> 9fc30ad... Upsample integers (inefficient), and smoother sky:src/shaders/march.fragment.glsl
 }
 
 int sdf(ivec3 c) {
@@ -83,6 +97,10 @@ March march( vec3 rayPos, vec3 rayDir, int MAX_STEPS ) {
 
   // Start marchin'
   while(res.step < MAX_STEPS && dist != 0) {
+<<<<<<<< HEAD:src/shader.fs
+========
+    int safeDist = max(1, dist-N); // works for some reason
+>>>>>>>> 9fc30ad... Upsample integers (inefficient), and smoother sky:src/shaders/march.fragment.glsl
 
 #ifdef MARCH_INTS
     for(int j = 0; j < max(1, dist/16 - 1); j++) {
@@ -133,10 +151,15 @@ void main() {
 
   vec2 screenPos = TexCoord / 2.;
 
+  vec3 camPos = iCamPos * float(N);
+  vec3 camRot = iCamRot;
+  vec3 rayDir;
+
   vec3 camDir = vec3(0, 1, 0);
   vec3 camPlaneU = vec3(1, 0, 0);
   vec3 camPlaneV = vec3(0, 0, 1) * iResolution.y / iResolution.x;
 
+<<<<<<<< HEAD:src/shader.fs
   vec3 rayDir = normalize(
       camDir
       + screenPos.x * camPlaneU
@@ -150,19 +173,50 @@ void main() {
   sunDir.xy = rotate2d(sunDir.xy, iTime);
 
   March res = march(camPos, rayDir, MAX_RAY_STEPS);
+========
+  if(TexCoord.x > 0.5 && TexCoord.y > 0.5) {
+    camPos = vec3(rotate2d(TexCoord-.625, camRot.z)*Yf + iCamPos.xy, Z);
+    rayDir = normalize(vec3(.01,.01,-1.));
+  } else {
+    rayDir = normalize(
+      camDir
+    + screenPos.x * camPlaneU
+    + screenPos.y * camPlaneV
+    );
+    rayDir.yz = rotate2d(rayDir.yz, camRot.x);
+    rayDir.xy = rotate2d(rayDir.xy, camRot.z);
+  }
+>>>>>>>> 9fc30ad... Upsample integers (inefficient), and smoother sky:src/shaders/march.fragment.glsl
+
+  vec3 sunDir = normalize(vec3(1,1,0.5));
+  sunDir.xy = rotate2d(sunDir.xy, iTime);
+
+  March res = march(camPos, rayDir, MAX_RAY_STEPS);
 
   // Start coloring
 
+<<<<<<<< HEAD:src/shader.fs
   FragColor = vec4(1);
+========
+  FragColor = vec4(0,0,0,1);
+// FragColor.rgb = mod(res.rayPos, 8.)/8.;
+>>>>>>>> 9fc30ad... Upsample integers (inefficient), and smoother sky:src/shaders/march.fragment.glsl
 
   vec3 baseCol = color(res.cellPos);
+//  FragColor.rgb = baseCol;
 
-  vec3 heightCol = (vec3(clamp(res.cellPos.z, 0, Z))/Zf + 5.)/6.;
+  vec3 heightCol = vec3(clamp(res.rayPos.z/Zf, 0., 1.) + 5.)/6.;
 
   vec3 shadeCol = mat3x3(
+<<<<<<<< HEAD:src/shader.fs
       0.6, 0.7, 0.8,
       0.7, 0.8, 1.0,
       1.0, 1.0, 1.0
+========
+      0.90, 0.95, 1.0,
+      0.90, 0.90, 1.0,
+      1.00, 1.00, 1.0
+>>>>>>>> 9fc30ad... Upsample integers (inefficient), and smoother sky:src/shaders/march.fragment.glsl
       ) * abs(res.normal);
 
   vec3 skyCol = mix(
@@ -173,18 +227,23 @@ void main() {
 
   vec3 ambCol = vec3(1);//mix(vec3(1), vec3(0.1, 0.2, 0.4), float(res.step)/MAX_RAY_STEPS);
 
-  float sunFactor = 0.;
-  if(dot(res.normal, sunDir) > 0.){
-    March sun = march(res.rayPos, sunDir, MAX_SUN_STEPS);
-    sunFactor = clamp(float(sun.minDist), 0., 1.);
+  float sunFactor = max(0., dot(res.normal, sunDir));
+  if( sunFactor > 0.){
+    March sun = march(vec3(res.rayPos) + vec3(0,0,1), sunDir, MAX_SUN_STEPS);
+    sunFactor *= clamp(float(sun.minDist), 0., 1.);
   }
   vec3 sunCol = mix(vec3(0.3, 0.5, 0.7), vec3(1), sunFactor);
 
   vec3 objCol = baseCol * shadeCol * heightCol * ambCol * sunCol;
 
   float dist = length(vec3(res.cellPos) - camPos);
+<<<<<<<< HEAD:src/shader.fs
   float skyFactor = (res.step == MAX_RAY_STEPS) ? 1.
     : clamp(pow(dist/Yf, 3.), 0., 1.);
+========
+  float skyFactor = (res.step == MAX_RAY_STEPS || res.minDist > Z) ? 1.
+    : clamp(abs(dist/Yf), 0., 1.);
+>>>>>>>> 9fc30ad... Upsample integers (inefficient), and smoother sky:src/shaders/march.fragment.glsl
 
   FragColor.rgb = mix( objCol, skyCol, skyFactor );
 
